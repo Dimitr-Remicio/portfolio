6d8591fb34c40d5b21e0c3b1c7e68c8556d66e6d
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-08-14 14:24:14 -0500

    image url fixed

--- src/components/Projects/ProjectItem/ProjectItem.jsx
+++ src/components/Projects/ProjectItem/ProjectItem.jsx
@@ -8,10 +8,7 @@ import { ASSETS_DIR } from '../../../enviroments';
 
 const ProjectItem = ({data, Context}) => {
 
-    // const body = document.querySelector('#body');
     const theme = useContext(Context);
-    const url = "/assets/images/";
-    // const view = "-preview-";
     
     return (
         // data.length > 1 ? <h1>error</h1> : 
@@ -37,8 +34,7 @@ const ProjectItem = ({data, Context}) => {
                     <div className={s.contitem__imgCont}id='Anim_tion' >
                         {
                             image.length > 0  ? (
-                                <img src={ASSETS_DIR + url + title +'-'+ theme + '.jpg'} id='Anim_tion' className={s.contitem__img} alt="image not found" />
-                                // <img src={image} id='Anim_tion' className={s.contitem__img} alt="image not found" />
+                                <img src={image} id='Anim_tion' className={s.contitem__img} alt="image not found" />
                                 )
                                 :
                                 (
