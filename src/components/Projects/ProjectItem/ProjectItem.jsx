d261b3cff7be08bea739f493ff2830baf8666c90
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-08-15 20:21:33 -0500

    last chance

--- src/components/Projects/ProjectItem/ProjectItem.jsx
+++ src/components/Projects/ProjectItem/ProjectItem.jsx
@@ -7,7 +7,7 @@ import { useContext } from 'react';
 const ProjectItem = ({ data, Context }) => {
 
   const theme = useContext(Context);
-  const image = `../../../public/assets/images/`;
+  const image = `/portfolio/assets/images/`;
 
   return data.map(
     ({ id, number, name, title, description, link, orientation }) => (
