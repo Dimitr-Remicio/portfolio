c54421632d9e12354c96807e67113506126c8504
Dimitr-remicio <dimitr.darm27@gmail.com>
2023-12-27 19:23:44 -0500

    update

--- src/components/home/HomeName.jsx
+++ src/components/home/HomeName.jsx
@@ -106,7 +106,7 @@ const Home = () => {
                 <img
                   className="imgen2 cybr"
                   alt="pic"
-                  src="public/assets/images/me3.png"
+                  src="public\assets\images\me3.png"
                 />
 
                 <div className="inner-element">
@@ -114,7 +114,7 @@ const Home = () => {
                     <img
                       className="imgen cybr-btn__glitch"
                       alt="pic"
-                      src="public/assets/images/me2.png"
+                      src="public\assets\images\me2.png"
                     />
                   </div>
                 </div>
