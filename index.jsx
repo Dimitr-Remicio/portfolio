03e7022f9f2c4349910e6bb365dd4330424ea896
Dimitr-remicio <dimitr.darm27@gmail.com>
2024-08-20 20:16:39 -0500

    update

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './src/App.jsx';
+import Headers from './src/features/head/head.jsx';
 
 // import Error from './error/Working.jsx';
 
--- src/components/Projects/ProjectItem/projectUsed.jsx
+++ src/components/Projects/ProjectItem/projectUsed.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import sprite from "../../../features/sprite.svg";
 
 function ProjectUsed({ data }) {
-  const items = JSON.stringify(data);
-  console.log(items);
+  // const items = JSON.stringify(data);
 
-  
+  const [item, setItem] = useState([]);
 
-  // return <div>{datarender}</div>;
+  useEffect(() => {
+    return () => {
+      setItem(data);
+    };
+  }, [data]);
+
+  console.log(item);
+
+  return (
+    <div class="Tag-PR">
+      {item.map((item, key) => {
+        return (
+          <div key={key} class="Tag-Item">
+            {item}
+            <div class="Tag-blr"></div>
+          <img src={`/public/assets/images/${item}.png`} width="22" height="22"/>
+          </div>
+        );
+      })}
+    </div>
+  );
 
   //   return `
   //       <P>${items}</P>
